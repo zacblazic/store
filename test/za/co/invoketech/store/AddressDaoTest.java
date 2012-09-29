e09f6acc3719ba6802d5ace4ab659488859f6386
Carel Nel <a.carel.g.nel@gmail.com>
2012-09-29 19:19:10 +0200

    Added (not yet working) AddressDao test

---
+++ test/za/co/invoketech/store/AddressDaoTest.java
@@ -0,0 +1,96 @@
+package za.co.invoketech.store;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import za.co.invoketech.store.application.config.ApplicationInitializer;
+import za.co.invoketech.store.application.config.PersistenceModule;
+import za.co.invoketech.store.model.address.Address;
+import za.co.invoketech.store.model.address.PhysicalAddress;
+import za.co.invoketech.store.model.address.PostOfficeBoxAddress;
+import za.co.invoketech.store.model.address.PrivateBagAddress;
+import za.co.invoketech.store.service.dao.AddressDao;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.persist.jpa.JpaPersistModule;
+
+
+public class AddressDaoTest {
+
+ private static final String PERSISTENCE_UNIT = "storeJpaUnit";
+ private static Injector injector;
+ private static AddressDao dao;
+ 
+ @BeforeClass
+ public static void setUpBeforeClass() throws Exception {
+  
+  injector = Guice.createInjector(new PersistenceModule(), new JpaPersistModule(PERSISTENCE_UNIT));
+  injector.getInstance(ApplicationInitializer.class);
+  dao = injector.getInstance(AddressDao.class);
+ }
+ 
+ @Test
+ public void test() {
+
+  List<Address> addressList = createAddresses();
+  List<Long> addressIds = new ArrayList<Long>();
+  testPersist(addressList, addressIds);
+ }
+ 
+ private List<Address> createAddresses(){
+  
+  List<Address> addressList = new ArrayList<Address>();
+  
+  PhysicalAddress home = injector.getInstance(PhysicalAddress.class);  
+  home.setUnitNumber("122");
+  home.setStreetName("Athens Road");
+  home.setSuburb("Table View");
+  home.setCity("Cape Town");
+  home.setCountry("South Africa");
+  home.setPostalCode("7441");
+  home.setFirstName("Zac");
+  home.setLastName("Blazic");
+  home.setPhoneNumber("+27215577102");
+  addressList.add(home);
+  
+  PostOfficeBoxAddress pobox = injector.getInstance(PostOfficeBoxAddress.class);
+  pobox.setBoxNumber("441");
+  pobox.setSuburb("Century City");
+  pobox.setCity("Cape Town");
+  pobox.setCountry("South Africa");
+  pobox.setPostalCode("7530");
+  pobox.setFirstName("Carel");
+  pobox.setLastName("Nel");
+  pobox.setPhoneNumber("+27722239110");
+  addressList.add(pobox);
+  
+  PrivateBagAddress pbag = injector.getInstance(PrivateBagAddress.class);
+  pbag.setBagNumber("747");
+  pbag.setSuburb("Zonnebloem");
+  pbag.setCity("Cape Town");
+  pbag.setCountry("South Africa");
+  pbag.setPostalCode("8000");
+  pbag.setFirstName("Ruan");
+  pbag.setLastName("Möller");
+  pbag.setPhoneNumber("+27768346153");
+  addressList.add(pbag);
+  
+  return addressList;
+ }
+ 
+ private void testPersist(List<Address> addressList, List<Long>addressIds) {
+  
+	for(int i = 0; i < addressList.size(); i++){
+   
+		dao.persist(addressList.get(i));
+		addressIds.add(addressList.get(i).getAddressId());
+		Address add = dao.findById(addressIds.get(i));
+		Assert.assertNotNull(dao.findById(addressIds.get(i)));
+	}
+ }
+}
