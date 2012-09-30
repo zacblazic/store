5fd0b6a1ca988554d65690b7557accbf09e44d61
Zac Blazic <zacblazic@gmail.com>
2012-09-30 17:33:06 +0200

    Added type saftey to the GenericDaoImpl by making use of TypedQuery<T>

--- src/za/co/invoketech/store/repository/dao/generic/GenericDao.java
+++ src/za/co/invoketech/store/repository/dao/generic/GenericDao.java
@@ -6,8 +6,8 @@ public interface GenericDao<T, ID> {
 
     public T findById(final ID id);
     public T findByAttribute(String attribute, String value);
-    public List<T> findEntitiesByAttribute(String attribute, String value);
     public List<T> findAll();
+    public List<T> findAllByAttribute(String attribute, String value);
     public void persist(final T entity);
     public void merge(final T entity);
     public void remove(final T entity);
