3cb7a043eae95a436254b381c56e14db1cd5690e
gconry18 <garethc18@gmail.com>
2012-10-05 16:53:08 +0200

    added service.customer package. added stubs for ShoppingCartService.

---
+++ src/za/co/invoketech/store/service/customer/internal/ShoppingCartServiceImpl.java
@@ -0,0 +1,7 @@
+package za.co.invoketech.store.service.customer.internal;
+
+import za.co.invoketech.store.service.customer.ShoppingCartService;
+
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+}
