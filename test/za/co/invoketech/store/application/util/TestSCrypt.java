ed2718906ac611411c5b24a4d55cc1786991282e
zblazic <zblazic@OB061.openbox.local>
2012-10-05 09:54:32 +0200

    Fixed problem that broke the SCrypt test

--- test/za/co/invoketech/store/application/util/TestSCrypt.java
+++ test/za/co/invoketech/store/application/util/TestSCrypt.java
@@ -2,23 +2,17 @@ package za.co.invoketech.store.application.util;
 
 public class TestSCrypt {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
-	String password = "mushki12";
-	int cpuCost = 16384;//16384;
-        int memoryCost = 64;//8;
-        int parallelization = 4;//1;
+		String password = "password";
+		int cpuCost = 16384;
+        int memoryCost = 64;
+        int parallelization = 4;
         
-        String hashedPassword1 = SCryptUtil.scrypt(password, cpuCost, memoryCost, parallelization);
-        String hashedPassword2 = SCryptUtil.scrypt(password, cpuCost, memoryCost, parallelization);
+        String hashedPassword = SCryptUtil.scrypt(password, cpuCost, memoryCost, parallelization);
         
         System.out.println("Password: " + password);
-        System.out.println("Hasshed password 1: " + hashedPassword1);
-        System.out.println("Hasshed password 2: " + hashedPassword2);
-        
-        System.out.println("Check status: " + SCrypt.check(password, hashedpassword1));
+        System.out.println("Hashed password: " + hashedPassword);
+        System.out.println("Check status: " + SCryptUtil.check(password, hashedPassword));
 	}
 }
