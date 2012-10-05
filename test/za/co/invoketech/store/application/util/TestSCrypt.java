4374e73094603da7ce472cfe4a47ee16ca7d1bce
Carel Nel <a.carel.g.nel@gmail.com>
2012-10-05 08:55:57 +0300

    Update test/za/co/invoketech/store/application/util/TestSCrypt.java
    Tweaked TestSCrypt.java to hash a password twice for comparison of the passwords.

--- test/za/co/invoketech/store/application/util/TestSCrypt.java
+++ test/za/co/invoketech/store/application/util/TestSCrypt.java
@@ -7,15 +7,17 @@ public class TestSCrypt {
 	 */
 	public static void main(String[] args) {
 
-		String password = "mushki12";
-		int cpuCost = 16384;//16384;
+	String password = "mushki12";
+	int cpuCost = 16384;//16384;
         int memoryCost = 64;//8;
-        int parallelization = 16;//1;
+        int parallelization = 4;//1;
         
-        String hashedPassword = SCryptUtil.scrypt(password, cpuCost, memoryCost, parallelization);
+        String hashedPassword1 = SCryptUtil.scrypt(password, cpuCost, memoryCost, parallelization);
+        String hashedPassword2 = SCryptUtil.scrypt(password, cpuCost, memoryCost, parallelization);
         
         System.out.println("Password: " + password);
-        System.out.println("Hasshed password: " + hashedPassword);
+        System.out.println("Hasshed password 1: " + hashedPassword1);
+        System.out.println("Hasshed password 2: " + hashedPassword2);
 	}
 
 }
