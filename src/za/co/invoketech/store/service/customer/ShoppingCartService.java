a5c562733be8b30806164efee869c9d2d4c1836d
gconry18 <garethc18@gmail.com>
2012-10-05 17:43:54 +0200

    Method stubs for shopping cart service.

--- src/za/co/invoketech/store/service/customer/ShoppingCartService.java
+++ src/za/co/invoketech/store/service/customer/ShoppingCartService.java
@@ -1,5 +1,9 @@
 package za.co.invoketech.store.service.customer;
 
 public interface ShoppingCartService {
-
+	public void addToCart(Long productId);
+	public void removeFromCart(Long shoppingCartItemId);
+	public void updateCart();
+	public void moveToWishlist();
+	public void checkout();
 }
--- src/za/co/invoketech/store/service/customer/internal/ShoppingCartServiceImpl.java
+++ src/za/co/invoketech/store/service/customer/internal/ShoppingCartServiceImpl.java
@@ -4,4 +4,34 @@ import za.co.invoketech.store.service.customer.ShoppingCartService;
 
 public class ShoppingCartServiceImpl implements ShoppingCartService {
 
+	@Override
+	public void addToCart(Long productId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFromCart(Long shoppingCartItemId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveToWishlist() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkout() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
