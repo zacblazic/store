ccb68d2ff51384c7b0a9dadfb106b0781242a468
gconry18 <garethc18@gmail.com>
2012-10-05 16:01:44 +0200

    Spacing Fix

--- src/za/co/invoketech/store/service/product/internal/ProductPricingServiceImpl.java
+++ src/za/co/invoketech/store/service/product/internal/ProductPricingServiceImpl.java
@@ -13,24 +13,21 @@ public class ProductPricingServiceImpl implements ProductPricingService {
 	private final float VAT_MARKUP = 1.14f;
 	
 	@Override
-	public BigDecimal setStandardMarkupPrice(BigDecimal supplierPrice) throws Exception{
-		if (supplierPrice.compareTo(new BigDecimal(0)) == -1 || supplierPrice.compareTo(new BigDecimal(0)) == 0 )
-		{
+	public BigDecimal setStandardMarkupPrice(BigDecimal supplierPrice) throws Exception {
+		
+		if (supplierPrice.compareTo(new BigDecimal(0)) == -1 || supplierPrice.compareTo(new BigDecimal(0)) == 0 ) {
 			throw new InvalidSupplierPriceException();
 		}
 		
 		float returnMarkup = 1.0f;
 			
-		if (supplierPrice.compareTo(new BigDecimal(200.0)) == -1 )
-		{
+		if (supplierPrice.compareTo(new BigDecimal(200.0)) == -1 ) {
 			returnMarkup += CHEAP_ITEM_MARKUP;
 		}
-		else if (supplierPrice.compareTo(new BigDecimal(1000.0)) == -1 )
-		{
+		else if (supplierPrice.compareTo(new BigDecimal(1000.0)) == -1 ) {
 			returnMarkup += MIDCOST_ITEM_MARKUP;
 		}
-		else if (supplierPrice.compareTo(new BigDecimal(1000.0)) == 0 || supplierPrice.compareTo(new BigDecimal(1000.0)) == 1)
-		{
+		else if (supplierPrice.compareTo(new BigDecimal(1000.0)) == 0 || supplierPrice.compareTo(new BigDecimal(1000.0)) == 1) {
 			returnMarkup += EXPENSIVE_ITEM_MARKUP;
 		}
 		
@@ -39,13 +36,12 @@ public class ProductPricingServiceImpl implements ProductPricingService {
 
 	@Override
 	public BigDecimal setCustomMarkupPrice(BigDecimal supplierPrice, float ratio) throws Exception {
-		if (supplierPrice.compareTo(new BigDecimal(0)) == -1 || supplierPrice.compareTo(new BigDecimal(0)) == 0 )
-		{
+		
+		if (supplierPrice.compareTo(new BigDecimal(0)) == -1 || supplierPrice.compareTo(new BigDecimal(0)) == 0 ) {
 			throw new InvalidSupplierPriceException();
 		}
 		
-		if (ratio <= 0 || ratio > 1)
-		{
+		if (ratio <= 0 || ratio > 1) {
 			throw new InvalidMarkupException();
 		}
 		
