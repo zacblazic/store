b362d1e1df0ea3dfb87ad9122aa9afb901ef8b1e
Zac Blazic <zacblazic@gmail.com>
2012-10-18 19:26:58 +0200

    Modified to fix pull issue

--- test/za/co/invoketech/store/service/file/FileServiceTest.java
+++ test/za/co/invoketech/store/service/file/FileServiceTest.java
@@ -13,14 +13,14 @@ public class FileServiceTest {
 		
 		System.out.println(System.getProperty("user.dir").toString());
 		
-//		String size = "large";
-//		String path = "C:/Users/Orion/Pictures/dhx-ddr3-angled - Copy.png";
-//		String productCode = "mem123";
-//		
-//		FileService.uploadImage(path, productCode, size);
-//		
-//		File image = new File("res/images/" + productCode + "/" + size + ".png");
-//		
-//		Assert.assertTrue(image.getAbsolutePath() + " does not exist", image.exists());
+		String size = "large";
+		//String path = "C:/Users/Orion/Pictures/dhx-ddr3-angled - Copy.png";
+		String productCode = "mem123";
+		
+		//FileService.uploadImage(path, productCode, size);
+		
+		File image = new File("res/images/" + productCode + "/" + size + ".png");
+		
+		Assert.assertTrue(image.getAbsolutePath() + " does not exist", image.exists());
 	}
 }
