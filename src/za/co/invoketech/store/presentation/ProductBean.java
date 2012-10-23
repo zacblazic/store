c880a4b617fbef6120c7c0dd8913e9fc968ca371
Carel Nel <a.carel.g.nel@gmail.com>
2012-10-23 20:49:42 +0200

    Further work on add product page and bean (uncompleted)

--- src/za/co/invoketech/store/presentation/ProductBean.java
+++ src/za/co/invoketech/store/presentation/ProductBean.java
@@ -17,25 +17,108 @@
 package za.co.invoketech.store.presentation;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import za.co.invoketech.store.presentation.category.Category;
+import za.co.invoketech.store.presentation.category.SimpleCategory;
 
 /**
  * @author a.carel.g.nel@gmail.com (Carel Nel)
  */
+
+@SessionScoped
+@ManagedBean
 public class ProductBean implements Serializable {
 		
 	private static final long serialVersionUID = 1L;
-	private Map<String,String> category = new HashMap<String, String>();
+	private List<Category> categories = new ArrayList<Category>();
+	private Category selected;
+	private String element;
 	
 	public ProductBean() {
-		category.put("Computer", "Computer");
-		category.put("Peripheral", "Peripheral");
-		category.put("Software", "Software");
-		category.put("Component", "Component");
+		System.out.println("construct method..........");
+		initialiseCategoryList();
 	}
 	
-	/*public String getCategory(String selection) {
-		//if()
-	}*/
+	public Category getSelected() {
+		System.out.println("get method.........." + selected);
+		return selected;
+	}
+	
+	public void setSelected(Category selected) {
+		System.out.println("set method..........");
+		this.selected = selected;
+	}
+	
+	public String getElement() {
+		return element;
+	}
+	
+	public void setElement(String element) {
+		this.element = element;
+	}
+	
+	public List<Category> getCategories() {
+		return categories;
+	}
+	
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+	
+	public void generateElement() {
+		System.out.println("generateElement method..........");
+		element = "<p:selectOneMenu id='category' value=''>\n" +
+				"<f:selectItem itemLabel='Select' itemValue='' />\n" +
+				"</p:selectOneMenu>";
+	}
+		
+	private void initialiseCategoryList() {
+		/*---------Component---------*/
+		categories.add(new SimpleCategory("Component"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Chassis"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Processor"));
+		categories.get(0).getSubCategory(1).addSubCategory(new SimpleCategory("Intel"));
+		categories.get(0).getSubCategory(1).addSubCategory(new SimpleCategory("AMD"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Motherboard"));
+		categories.get(0).getSubCategory(2).addSubCategory(new SimpleCategory("Intel"));
+		categories.get(0).getSubCategory(2).addSubCategory(new SimpleCategory("AMD"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Graphics Card"));
+		categories.get(0).getSubCategory(3).addSubCategory(new SimpleCategory("Nvidia"));
+		categories.get(0).getSubCategory(3).addSubCategory(new SimpleCategory("ATi"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Memory"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Power Supply"));
+		
+		categories.get(0).addSubCategory(new SimpleCategory("Internal Storage"));
+		categories.get(0).getSubCategory(6).addSubCategory(new SimpleCategory("Hard Disk Drive"));
+		categories.get(0).getSubCategory(6).addSubCategory(new SimpleCategory("Solid State Drive"));
+		
+		/*---------Computer---------*/
+		categories.add(new SimpleCategory("Computer"));
+		categories.get(1).addSubCategory(new SimpleCategory("Custom Computer"));
+		categories.get(1).addSubCategory(new SimpleCategory("Pre-Built Computer"));
+		
+		/*---------Peripheral---------*/		
+		categories.add(new SimpleCategory("Peripheral"));
+		categories.get(2).addSubCategory(new SimpleCategory("Audio"));
+		categories.get(2).addSubCategory(new SimpleCategory("Display"));
+		categories.get(2).addSubCategory(new SimpleCategory("Keyboard"));
+		categories.get(2).addSubCategory(new SimpleCategory("Mouse"));
+		
+		/*---------Software---------*/
+		categories.add(new SimpleCategory("Software"));
+		categories.get(3).addSubCategory(new SimpleCategory("Anti Virus"));
+		categories.get(3).addSubCategory(new SimpleCategory("Game"));
+		categories.get(3).addSubCategory(new SimpleCategory("Operating System"));
+	}
 }
---
+++ test/za/co/invoketech/store/CategoryTest.java
@@ -0,0 +1,72 @@
+package za.co.invoketech.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import za.co.invoketech.store.presentation.category.Category;
+import za.co.invoketech.store.presentation.category.SimpleCategory;
+
+public class CategoryTest {
+
+	@Test
+	public void test() {
+		
+		List<Category> components = new ArrayList<Category>();
+
+		/*---------Component---------*/
+		components.add(new SimpleCategory("Component"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Chassis"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Processor"));
+		components.get(0).getSubCategory(1).addSubCategory(new SimpleCategory("Intel"));
+		components.get(0).getSubCategory(1).addSubCategory(new SimpleCategory("AMD"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Motherboard"));
+		components.get(0).getSubCategory(2).addSubCategory(new SimpleCategory("Intel"));
+		components.get(0).getSubCategory(2).addSubCategory(new SimpleCategory("AMD"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Graphics Card"));
+		components.get(0).getSubCategory(3).addSubCategory(new SimpleCategory("Nvidia"));
+		components.get(0).getSubCategory(3).addSubCategory(new SimpleCategory("ATi"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Memory"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Power Supply"));
+		
+		components.get(0).addSubCategory(new SimpleCategory("Internal Storage"));
+		components.get(0).getSubCategory(6).addSubCategory(new SimpleCategory("Hard Disk Drive"));
+		components.get(0).getSubCategory(6).addSubCategory(new SimpleCategory("Solid State Drive"));
+		
+		/*---------Computer---------*/
+		components.add(new SimpleCategory("Computer"));
+		components.get(1).addSubCategory(new SimpleCategory("Custom Computer"));
+		components.get(1).addSubCategory(new SimpleCategory("Pre-Built Computer"));
+		
+		/*---------Peripheral---------*/		
+		components.add(new SimpleCategory("Peripheral"));
+		components.get(2).addSubCategory(new SimpleCategory("Audio"));
+		components.get(2).addSubCategory(new SimpleCategory("Display"));
+		components.get(2).addSubCategory(new SimpleCategory("Keyboard"));
+		components.get(2).addSubCategory(new SimpleCategory("Mouse"));
+		
+		/*---------Software---------*/
+		components.add(new SimpleCategory("Software"));
+		components.get(3).addSubCategory(new SimpleCategory("Anti Virus"));
+		components.get(3).addSubCategory(new SimpleCategory("Game"));
+		components.get(3).addSubCategory(new SimpleCategory("Operating System"));
+		
+		
+		for(Category c : components) {
+			System.out.println("L1 - " + c);
+			for(Category g : c.getSubCategories()) {
+				System.out.println("\tL2 - " + g.getName());
+				for(Category f : g.getSubCategories()) {
+					System.out.println("\t\tL3 - " + f.getName());
+				}
+			}
+		}
+	}
+}
