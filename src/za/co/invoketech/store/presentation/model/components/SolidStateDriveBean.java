8c9818637418ee881c3e61fd67ea02fba7e6b740
Carel Nel <a.carel.g.nel@gmail.com>
2012-10-29 10:56:29 +0200

    new hdd page and hdd and ssd bean

---
+++ src/za/co/invoketech/store/presentation/model/components/SolidStateDriveBean.java
@@ -0,0 +1,182 @@
+/**
+ * Copyright (c) 2012 Invoke Tech
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package za.co.invoketech.store.presentation.model.components;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import za.co.invoketech.store.application.config.Goose;
+import za.co.invoketech.store.domain.model.product.Brand;
+import za.co.invoketech.store.domain.model.product.component.SolidStateDrive;
+import za.co.invoketech.store.service.product.ProductService;
+import za.co.invoketech.store.service.repository.BrandRepository;
+
+import com.google.inject.Inject;
+
+
+/**
+ * @author a.carel.g.nel@gmail.com (Carel Nel)
+ */
+
+@SessionScoped
+@ManagedBean
+public class SolidStateDriveBean implements Serializable {
+
+	@Inject
+	private BrandRepository brandRepository;
+	@Inject
+	private ProductService productService;
+	
+	private static final long serialVersionUID = 1L;
+	private List<Brand> brands = new ArrayList<Brand>();
+	private String theBrand;
+	private String title;
+	private long stock;
+	private double price;
+	private Date discontinuedDate;
+	private String capacity;
+	private String formFactor;
+	private String bus;
+	private int cache;
+	private int readSpeed;
+	private int writeSpeed;
+	
+	public SolidStateDriveBean() {
+		Goose.guicify(this);
+		brands = brandRepository.findAll();
+	}
+
+	public List<Brand> getBrands() {
+		return brands;
+	}
+
+	public void setBrands(List<Brand> brands) {
+		this.brands = brands;
+	}
+
+	public String getTheBrand() {
+		return theBrand;
+	}
+
+	public void setTheBrand(String theBrand) {
+		this.theBrand = theBrand;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public long getStock() {
+		return stock;
+	}
+
+	public void setStock(long stock) {
+		this.stock = stock;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Date getDiscontinuedDate() {
+		return discontinuedDate;
+	}
+
+	public void setDiscontinuedDate(Date discontinuedDate) {
+		this.discontinuedDate = discontinuedDate;
+	}
+
+	public String getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(String capacity) {
+		this.capacity = capacity;
+	}
+
+	public String getFormFactor() {
+		return formFactor;
+	}
+
+	public void setFormFactor(String formFactor) {
+		this.formFactor = formFactor;
+	}
+
+	public String getBus() {
+		return bus;
+	}
+
+	public void setBus(String bus) {
+		this.bus = bus;
+	}
+
+	public int getCache() {
+		return cache;
+	}
+
+	public void setCache(int cache) {
+		this.cache = cache;
+	}
+
+	public int getReadSpeed() {
+		return readSpeed;
+	}
+
+	public void setReadSpeed(int readSpeed) {
+		this.readSpeed = readSpeed;
+	}
+
+	public int getWriteSpeed() {
+		return writeSpeed;
+	}
+
+	public void setWriteSpeed(int writeSpeed) {
+		this.writeSpeed = writeSpeed;
+	}
+	
+	public void insert() {
+		
+		SolidStateDrive ssd = new SolidStateDrive();
+		ssd.setTitle(title);
+		ssd.setStock(stock);
+		ssd.setPrice(new BigDecimal(price));
+		ssd.setCapacity(capacity);
+		ssd.setFormFactor(formFactor);
+		ssd.setBus(bus);
+		ssd.setCache(cache);
+		ssd.setReadSpeed(readSpeed);
+		ssd.setWriteSpeed(writeSpeed);
+		
+		Brand brand = new Brand(theBrand);
+		
+		productService.insertProduct(ssd, brand);
+	}
+}
