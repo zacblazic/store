ed678931faee14867ea39c8e59ee8cc87adeb3c0
gconry18 <garethc18@gmail.com>
2012-10-29 03:18:19 +0200

    Added security for account folder in shiro config. Added copyright and author comments to rest files

--- src/za/co/invoketech/store/integration/webservices/rest/server/InvokeRestServer.java
+++ src/za/co/invoketech/store/integration/webservices/rest/server/InvokeRestServer.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2012 Invoke Tech
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package za.co.invoketech.store.integration.webservices.rest.server;
 
 import org.restlet.Application;
@@ -10,8 +25,14 @@ import za.co.invoketech.store.integration.webservices.rest.server.resource.Order
 import za.co.invoketech.store.integration.webservices.rest.server.resource.OrdersResource;
 import za.co.invoketech.store.integration.webservices.rest.server.resource.TestResource;
 
+/**
+ * 
+ * @author garethc18@gmail.com (Gareth Conry)
+ *
+ */
 public class InvokeRestServer extends Application {
 	
+	// For Running server independent of a container
 	public static void runServer(int port) throws Exception {
 		// Create a component.  
 		Component component = new Component();
--- src/za/co/invoketech/store/integration/webservices/rest/server/resource/OrdersResource.java
+++ src/za/co/invoketech/store/integration/webservices/rest/server/resource/OrdersResource.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2012 Invoke Tech
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package za.co.invoketech.store.integration.webservices.rest.server.resource;
 
 import java.util.List;
@@ -13,6 +28,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.Inject;
 
+/**
+ * 
+ * @author garethc18@gmail.com (Gareth Conry)
+ *
+ */
 public class OrdersResource extends ServerResource {
 	@Inject
 	private OrderRepository orderRepository;
