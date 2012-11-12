8cdd13df58cc8a96369927b8f7ff82425945d989
gconry18 <garethc18@gmail.com>
2012-11-13 00:11:01 +0200

    More shopping cart service stubs, Changes to AccountService

--- src/za/co/invoketech/store/service/cart/ShoppingCartService.java
+++ src/za/co/invoketech/store/service/cart/ShoppingCartService.java
@@ -2,9 +2,12 @@ package za.co.invoketech.store.service.cart;
 
 import za.co.invoketech.store.domain.model.cart.ShoppingCartItem;
 import za.co.invoketech.store.domain.model.customer.Customer;
+import za.co.invoketech.store.domain.model.order.DeliveryAddress;
 import za.co.invoketech.store.domain.model.product.Product;
 
 public interface ShoppingCartService {
 	public void addToCustomerCart (Customer customer, ShoppingCartItem item);
 	public void addToCustomerCart (Customer customer, Product product, int quantity);
+	public void checkout(Customer customer, DeliveryAddress address);
+	public void updateQuantity(ShoppingCartItem shoppingCartItem, int quantity);
 }
--- src/za/co/invoketech/store/service/internal/ShoppingCartServiceImpl.java
+++ src/za/co/invoketech/store/service/internal/ShoppingCartServiceImpl.java
@@ -2,6 +2,7 @@ package za.co.invoketech.store.service.internal;
 
 import za.co.invoketech.store.domain.model.cart.ShoppingCartItem;
 import za.co.invoketech.store.domain.model.customer.Customer;
+import za.co.invoketech.store.domain.model.order.DeliveryAddress;
 import za.co.invoketech.store.domain.model.product.Product;
 import za.co.invoketech.store.service.cart.ShoppingCartService;
 
@@ -18,6 +19,18 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 			int quantity) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void checkout(Customer customer, DeliveryAddress address) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateQuantity(ShoppingCartItem shoppingCartItem, int quantity) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
