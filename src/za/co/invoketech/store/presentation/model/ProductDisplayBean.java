b9dc993174da18d50861c69d9e29b08609cbd51b
Carel Nel <a.carel.g.nel@gmail.com>
2012-11-15 01:37:05 +0200

    further development on product page

---
+++ src/za/co/invoketech/store/presentation/model/ProductDisplayBean.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2012 Invoke Tech
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package za.co.invoketech.store.presentation.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * @author a.carel.g.nel@gmail.com (Carel Nel)
+ */
+
+@SessionScoped
+@ManagedBean
+public class ProductDisplayBean implements Serializable {
+		
+	private static final long serialVersionUID = 1L;
+	private String image;
+	private String title;
+	private long id;
+	private BigDecimal price;
+	private long stock;
+	
+	public ProductDisplayBean() {
+		image = "/images/123456/img_123456_1.jpg";
+		title = "test";
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImages(String image) {
+		this.image = image;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public long getStock() {
+		return stock;
+	}
+
+	public void setStock(long stock) {
+		this.stock = stock;
+	}
+}
