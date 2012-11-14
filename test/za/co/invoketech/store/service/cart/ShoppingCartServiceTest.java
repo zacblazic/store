37116d4b45ca0944baba06e7f3f7c5d3a9df7c4a
gconry18 <garethc18@gmail.com>
2012-11-14 15:11:23 +0200

    Carel is pushing the test in order to have a look at it on his own PC

---
+++ test/za/co/invoketech/store/service/cart/ShoppingCartServiceTest.java
@@ -0,0 +1,209 @@
+package za.co.invoketech.store.service.cart;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import za.co.invoketech.store.application.config.Goose;
+import za.co.invoketech.store.application.exception.InsufficientStockException;
+import za.co.invoketech.store.domain.model.account.Account;
+import za.co.invoketech.store.domain.model.cart.ShoppingCartItem;
+import za.co.invoketech.store.domain.model.customer.Address;
+import za.co.invoketech.store.domain.model.customer.Customer;
+import za.co.invoketech.store.domain.model.order.DeliveryAddress;
+import za.co.invoketech.store.domain.model.order.Order;
+import za.co.invoketech.store.domain.model.product.component.Memory;
+import za.co.invoketech.store.domain.shared.AddressType;
+import za.co.invoketech.store.domain.shared.Gender;
+import za.co.invoketech.store.domain.shared.InternalAddress;
+import za.co.invoketech.store.domain.shared.Person;
+import za.co.invoketech.store.service.account.AccountService;
+import za.co.invoketech.store.service.account.RoleService;
+import za.co.invoketech.store.service.customer.CustomerService;
+import za.co.invoketech.store.service.repository.CustomerRepository;
+import za.co.invoketech.store.service.repository.OrderRepository;
+import za.co.invoketech.store.service.repository.ProductRepository;
+
+import com.google.inject.Injector;
+/**
+ * 
+ * @author garethc18@gmail.com (Gareth Conry)
+ *
+ */
+public class ShoppingCartServiceTest {
+	private static Injector injector;
+	 
+	private static RoleService roleService;
+	private static AccountService accountService;
+	private static CustomerService customerService;
+	private static ProductRepository productRepository;
+	private static CustomerRepository customerRepository;
+	private static ShoppingCartService shoppingCartService;
+	private static OrderRepository orderRepository;
+	
+	private static Person peterPerson;
+	private static Account peterCustAcc;
+	private static Customer peterCustomer;
+	private static Memory memoryInStock;
+	private static Memory memoryNoStock;
+	
+	private static Address createAddress() {
+		InternalAddress internalAddress = new InternalAddress.Builder()
+			.firstName("Zac")
+			.lastName("Blazic")
+			.phoneNumber("0828943000")
+			.line1("122 Athens Road")
+			.line2("Table View")
+			.city("Cape Town")
+			.postalCode("7441")
+			.country("South Africa")
+			.addressType(AddressType.PHYSICAL).build();
+		return new Address("Home", internalAddress);
+	}
+	private static DeliveryAddress createDeliveryAddress() {
+		InternalAddress internalAddress = new InternalAddress.Builder()
+			.firstName("Zac")
+			.lastName("Blazic")
+			.phoneNumber("0828943000")
+			.line1("122 Athens Road")
+			.line2("Table View")
+			.city("Cape Town")
+			.postalCode("7441")
+			.country("South Africa")
+			.addressType(AddressType.PHYSICAL).build();
+		return new DeliveryAddress(internalAddress);
+	}
+
+	public ShoppingCartServiceTest() {
+		
+	}
+	
+	@BeforeClass
+	public static void createData() throws Exception {
+		injector = Goose.getInjectorForTesting();
+		
+		roleService = injector.getInstance(RoleService.class);
+		accountService = injector.getInstance(AccountService.class);
+		customerService = injector.getInstance(CustomerService.class);
+		productRepository = injector.getInstance(ProductRepository.class);
+		customerRepository = injector.getInstance(CustomerRepository.class);
+		shoppingCartService = injector.getInstance(ShoppingCartService.class);
+		orderRepository = injector.getInstance(OrderRepository.class);
+		
+		/* Create Data */
+		// Customer Data		
+		peterPerson = new Person("Peter", "Parker", Gender.MALE, "0833512456");
+		peterCustAcc = new Account("pparker@gmail.com", "spiderman");
+		peterCustomer = customerService.createCustomer(peterPerson, createAddress(), peterCustAcc);
+		peterCustAcc = peterCustomer.getAccount();
+		
+		// Product Data		
+		memoryInStock = new Memory();
+		memoryInStock.setTitle("Corsair Vengeance LP 8GB");
+		memoryInStock.setPrice(new BigDecimal(478));
+		memoryInStock.setStock(5);
+		memoryInStock.setFrequency("1600");
+		memoryInStock.setLatency("9-9-9-24");
+		memoryInStock.setModules(2);
+		
+		productRepository.persist(memoryInStock);
+
+		memoryNoStock = new Memory();
+		memoryNoStock.setTitle("Corsair Vengeance LP 16GB");
+		memoryNoStock.setPrice(new BigDecimal(478));
+		memoryNoStock.setStock(0);
+		memoryNoStock.setFrequency("1600");
+		memoryNoStock.setLatency("9-9-9-24");
+		memoryNoStock.setModules(4);		
+
+		productRepository.persist(memoryNoStock);
+	}
+	
+	@AfterClass
+	public static void removeData() throws Exception {
+		customerRepository.remove(peterCustomer);
+		peterCustAcc.setCustomer(null);
+		accountService.removeAccount(peterCustAcc);
+		productRepository.remove(memoryInStock);
+		productRepository.remove(memoryNoStock);
+	}
+	
+	@After
+	public void afterTest() {
+		peterCustomer.getShoppingCart().removeAllItems();
+		
+		if (peterCustomer.getOrders().size() != 0) {
+			for (Order order : peterCustomer.getOrders()) {
+				orderRepository.remove(order);
+			}
+		}
+	}
+
+	@Test
+	public void testAddProductToCartWithStock() {
+		ShoppingCartItem shoppingCartItem = new ShoppingCartItem(memoryInStock);
+		shoppingCartService.addToCustomerCart(peterCustomer, shoppingCartItem);
+		
+		boolean incart = false;
+		List<ShoppingCartItem> itemsInCart = peterCustomer.getShoppingCart().getItems();
+		for (ShoppingCartItem item : itemsInCart) {
+			if (item.getId() == memoryInStock.getId()){
+				incart = true;
+				break;
+			}
+		}
+		
+		Assert.assertTrue(incart);
+	}
+
+	@Test (expected=InsufficientStockException.class)
+	public void testAddProductToCartNoStock() {
+		ShoppingCartItem shoppingCartItem = new ShoppingCartItem(memoryNoStock);
+		shoppingCartService.addToCustomerCart(peterCustomer, shoppingCartItem);
+		
+		Assert.fail("No Exception Thrown");
+	}
+	
+	@Test
+	public void testUpdateQtyValid() {
+		ShoppingCartItem shoppingCartItem = new ShoppingCartItem(memoryInStock);
+		shoppingCartService.addToCustomerCart(peterCustomer, shoppingCartItem);
+		
+		shoppingCartService.updateQuantity(shoppingCartItem, 3);
+		
+		Assert.assertTrue(peterCustomer.getShoppingCart().getItems().get(0).getQuantity() == 3);
+	}
+
+	@Test (expected=InsufficientStockException.class)
+	public void testUpdateQtyInvalid() {
+		ShoppingCartItem shoppingCartItem = new ShoppingCartItem(memoryInStock);
+		shoppingCartService.addToCustomerCart(peterCustomer, shoppingCartItem);
+		
+		shoppingCartService.updateQuantity(shoppingCartItem, 6);
+		
+		Assert.fail("No Exception Thrown");
+	}
+	
+	@Test
+	public void testCheckout() {
+		ShoppingCartItem shoppingCartItem = new ShoppingCartItem(memoryInStock, 1);
+		shoppingCartService.addToCustomerCart(peterCustomer, shoppingCartItem);
+		
+		shoppingCartService.checkout(peterCustomer, createDeliveryAddress());
+		
+		if (peterCustomer.getOrders().size() != 0) {
+			for (Order order : peterCustomer.getOrders()) {
+				Assert.assertTrue(order.getItems().get(0).getProduct().equals(memoryInStock));
+			}
+		}
+		else {
+			Assert.fail("Order Not Created");
+		}
+	}
+}
