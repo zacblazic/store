153425997316fc6aff60cc81b330745195db4aa5
gconry18 <garethc18@gmail.com>
2012-11-15 23:14:54 +0200

    ShoppingCartService Checkout Complete and Added returns for customer ShoppingCart or List<ShoppingCartItem> ShoppingCartServiceTest working.

--- src/za/co/invoketech/store/service/cart/ShoppingCartService.java
+++ src/za/co/invoketech/store/service/cart/ShoppingCartService.java
@@ -1,15 +1,22 @@
 package za.co.invoketech.store.service.cart;
 
+import java.util.List;
+
 import za.co.invoketech.store.application.exception.CustomerNotFoundException;
 import za.co.invoketech.store.application.exception.InvalidStockException;
 import za.co.invoketech.store.application.exception.ProductNotFoundException;
 import za.co.invoketech.store.application.exception.ShoppingCartItemNotFoundException;
+import za.co.invoketech.store.domain.model.cart.ShoppingCart;
 import za.co.invoketech.store.domain.model.cart.ShoppingCartItem;
+import za.co.invoketech.store.domain.model.order.DeliveryMethod;
+import za.co.invoketech.store.domain.model.order.Payment;
 
 public interface ShoppingCartService {
+	public ShoppingCart customerCart (long customerId) throws CustomerNotFoundException;
+	public List<ShoppingCartItem> customerCartItems (long customerId) throws CustomerNotFoundException;
 	public void addToCustomerCart (long customerId, ShoppingCartItem item) throws InvalidStockException, ProductNotFoundException, CustomerNotFoundException;
 	public void addToCustomerCart (long customerId, long productId, int quantity) throws InvalidStockException, ProductNotFoundException, CustomerNotFoundException;
 	public void updateQuantity(long customerId, long shoppingCartItemId, int quantity) 
 			throws ShoppingCartItemNotFoundException, CustomerNotFoundException, InvalidStockException, ProductNotFoundException;
-	public void checkout(long customerId, long addressId);
+	public void checkout(long customerId, long addressId, Payment payment, DeliveryMethod method) throws CustomerNotFoundException;
 }
--- src/za/co/invoketech/store/service/internal/ShoppingCartServiceImpl.java
+++ src/za/co/invoketech/store/service/internal/ShoppingCartServiceImpl.java
@@ -1,16 +1,29 @@
 package za.co.invoketech.store.service.internal;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import za.co.invoketech.store.application.exception.CustomerNotFoundException;
 import za.co.invoketech.store.application.exception.InvalidStockException;
 import za.co.invoketech.store.application.exception.ProductNotFoundException;
 import za.co.invoketech.store.application.exception.ShoppingCartItemNotFoundException;
 import za.co.invoketech.store.domain.model.cart.ShoppingCart;
 import za.co.invoketech.store.domain.model.cart.ShoppingCartItem;
+import za.co.invoketech.store.domain.model.customer.Address;
 import za.co.invoketech.store.domain.model.customer.Customer;
+import za.co.invoketech.store.domain.model.order.Delivery;
+import za.co.invoketech.store.domain.model.order.DeliveryAddress;
+import za.co.invoketech.store.domain.model.order.DeliveryMethod;
+import za.co.invoketech.store.domain.model.order.Order;
+import za.co.invoketech.store.domain.model.order.OrderItem;
+import za.co.invoketech.store.domain.model.order.Payment;
 import za.co.invoketech.store.domain.model.product.Product;
+import za.co.invoketech.store.service.address.AddressService;
 import za.co.invoketech.store.service.cart.ShoppingCartService;
 import za.co.invoketech.store.service.customer.CustomerService;
 import za.co.invoketech.store.service.product.ProductService;
+import za.co.invoketech.store.service.repository.OrderRepository;
 
 import com.google.inject.Inject;
 
@@ -21,6 +34,26 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 	
 	@Inject
 	private CustomerService customerService;
+	
+	@Inject
+	private AddressService addressService;
+	
+	@Inject
+	private OrderRepository orderRepository;
+
+	@Override
+	public ShoppingCart customerCart(long customerId) throws CustomerNotFoundException {
+		Customer customer = customerService.findCustomerById(customerId);
+		ShoppingCart cart = customer.getShoppingCart();
+		return cart;
+	}
+
+	@Override
+	public List<ShoppingCartItem> customerCartItems(long customerId) throws CustomerNotFoundException {
+		Customer customer = customerService.findCustomerById(customerId);
+		ShoppingCart cart = customer.getShoppingCart();
+		return cart.getItems();
+	}
 
 	@Override
 	public void addToCustomerCart(long customerId, ShoppingCartItem item) throws InvalidStockException, ProductNotFoundException, CustomerNotFoundException {
@@ -87,9 +120,44 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 	}
 
 	@Override
-	public void checkout(long customerId, long addressId) {
-		// TODO Auto-generated method stub
+	public void checkout(long customerId, long addressId, Payment payment, DeliveryMethod method) throws CustomerNotFoundException {
+
+		Customer customer = customerService.findCustomerById(customerId);
 		
+		ShoppingCart cart = customer.getShoppingCart();
+		List<ShoppingCartItem> sciList = cart.getItems();
+		
+		List<OrderItem> items = new ArrayList<>();
+		
+		for (ShoppingCartItem sci : sciList) {
+			OrderItem newItem = new OrderItem(sci.getProduct(), sci.getProduct().getUnitPrice(), sci.getQuantity());
+			items.add(newItem);
+		}
+		
+		Address address = null;
+		List<Address> addressList = customer.getAddresses();
+		for (Address addr : addressList) {
+			if (addr.getId() == addressId) {
+				address = addr;
+			}
+		}
+		
+		DeliveryAddress delAddr = addressService.createDeliveryAddress(address);
+		
+		BigDecimal cost = new BigDecimal(0);		
+		
+		Delivery delivery = new Delivery(method, delAddr, cost);
+		
+		Order order = new Order(customer, items, payment, delivery);
+		orderRepository.persist(order);
+		
+		List<Order> orders = customer.getOrders();
+		orders.add(order);
+		
+		cart.removeAllItems();
+		customer.setShoppingCart(cart);
+		customer.setOrders(orders);
+		customerService.updateCustomer(customer);
 	}
 
 }
